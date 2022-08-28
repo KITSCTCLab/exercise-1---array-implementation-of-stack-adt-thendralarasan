c50082640dac0ca6fff1ac111a8c0db7650f865a
thendralarasan <110447168+thendralarasan@users.noreply.github.com>
2022-08-28 17:56:01 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,31 +1,49 @@
 class Stack:
     def __init__(self, size):
-        self.items = [None]*size
-        self.size = size
-        self.top = -1
+       self.size = size
+       self.lst = [None]*size
+       self.top = -1 
+
     def is_empty(self):
-        if (self.top == -1):
-            return True
-        else:
-            return False
         # Write code here
+        if self.top == -1 :
+            return 1
+        else :
+            return 0
+
     def is_full(self):
-        if (self.top == self.size-1):
-           return True
-        else:
-           return False
         # Write code here
+        if self.top == (self.size - 1) :
+            return 1
+        else :
+            return 0
+
     def push(self, data):
         if not self.is_full():
-            self.top += 1
-            self.items[self.top] = data
             # Write code here
+            self.top+=1
+            self.lst[self.top]=data
+
     def pop(self):
         if not self.is_empty():
-            data = self.items[self.top]
-            self.top -= 1
-            return data
             # Write code here
+            del self.lst[self.top]
+            self.top-=1
+            
+
     def status(self):
-        for i in range(self.top+1):
-            print(self.items[i])
+        # Write code here
+         if not self.is_empty():
+            for i in range(0,self.top+1):
+                print(self.lst[i])
+
+# Do not change the following code
+size, queries = map(int, input().rstrip().split())
+stack = Stack(size)
+for line in range(queries):
+    values = list(map(int, input().rstrip().split()))
+    if values[0] == 1:
+        stack.push(values[1])
+    elif values[0] == 2:
+        stack.pop()
+stack.status()
